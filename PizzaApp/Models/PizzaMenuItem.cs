43320507057a6a5cbd2ceadd787ba76544e08a2a
Hatt736 <drewdood736@gmail.com>
2022-04-30 14:15:41 -0400

    Header

--- PizzaApp/Models/PizzaMenuItem.cs
+++ PizzaApp/Models/PizzaMenuItem.cs
@@ -10,6 +10,7 @@ namespace PizzaApp.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Category { get; set; }
         public double Price { get; set; }
     }
 }
