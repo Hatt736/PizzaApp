5f461a51483d4e1ff15384f4f8074bb1a813c645
Billy Martin <billy@sitesculptors.com>
2022-05-03 08:42:39 -0400

    Flyout menu works

--- PizzaApp/AppShell.xaml.cs
+++ PizzaApp/AppShell.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using PizzaApp.Views;
 
+using System.Windows.Input;
+
 namespace PizzaApp;
 
 public partial class AppShell : Shell
@@ -8,7 +10,30 @@ public partial class AppShell : Shell
     {
         InitializeComponent();
 
+        BindingContext = this;
+
         Routing.RegisterRoute(nameof(OrderPage), typeof(OrderPage));
         Routing.RegisterRoute(nameof(CheckoutPage), typeof(CheckoutPage));
+    }
+
+    private bool isFlyoutOpen;
+    public bool IsFlyoutOpen
+    {
+        get { return isFlyoutOpen; }
+        set
+        {
+            isFlyoutOpen = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private ICommand closeFlyoutCommand;
+    public ICommand CloseFlyoutCommand =>
+        closeFlyoutCommand ??
+        (closeFlyoutCommand = new Command<object>((x) => ExecuteCloseFlyoutCommand(x)));
+
+    private void ExecuteCloseFlyoutCommand(object x)
+    {
+        IsFlyoutOpen = false;
     }
 }
